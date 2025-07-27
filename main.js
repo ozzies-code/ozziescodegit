30f0cf2a877846e98be37be81b3ac7f1a3c2bae9
oswaldojmp <oswaldojesusmarinpages@gmail.com>
2025-07-27 16:00:31 -0400

    branch master  updated

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-const a = "rama feature-branch-2";
+const a = "rama master-branch";
+
+while (condition) {
+    // some logic
+    if (someCondition) {
+        a = "rama feature-branch-2";
+        break; // exit the loop if condition is met
+    }
+    // more logic
+    
+}
 
 console.log(a);
